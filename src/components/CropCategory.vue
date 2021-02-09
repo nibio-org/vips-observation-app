e728785cec682344a5b816fed600d89fda0f584b
Bhabesh <bhabesh.mukhopadhyay@nibio.no>
2021-02-09 12:10:53 +0100

    Checkboxes for categoryid
    v-bind  categoryId with checkbox and attached with v-model to get the selected data

--- src/components/CropCategory.vue
+++ src/components/CropCategory.vue
@@ -3,20 +3,26 @@
   <div id="divCropCategory">
     <h1>Crop Category List</h1>
     <li v-for="item in listCropCategoris">
-      {{ item.defaultName }}
+      <input type="checkbox" v-bind:value='item.cropCategoryId' v-model="listSelectedCropCategoris"> {{ item.defaultName }}
     </li>
+    <span>Checked names: {{ listSelectedCropCategoris }}</span>
   </div>
 </template>
 
 <script>
 export default {
   name: "cropCategory",
-  data: {
-    listCropCategoris: []
-    //return listCropCategoris;
+  data() {
+    {listSelectedCropCategoris : []};
+    return {
+      listSelectedCropCategoris:[],
+      listCropCategoris: []
+      }
+
   },
   methods: {
     funFetchCropCategory(organizationId) {
+//        this.$nextTick(function () {
       fetch(
         "https://logic.vips.nibio.no/rest/organism/cropcategory/" +
           organizationId
@@ -33,6 +39,7 @@ export default {
       .then((data) => {
         this.listCropCategoris = data;
       });
+ //   })
     },
   },
   mounted()
