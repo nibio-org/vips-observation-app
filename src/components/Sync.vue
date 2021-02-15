f923c7a027d51228c69866208e807b7c99a5b466
Bhabesh <bhabesh.mukhopadhyay@nibio.no>
2021-02-15 17:32:27 +0100

    Initiation of Sync Component
    For Synchronization

---
+++ src/components/Sync.vue
@@ -0,0 +1,35 @@
+<template></template>
+
+<script>
+export default {
+    name: "Sync",
+
+    data() {
+        return {
+            strServerTimeStamp : ''
+        }
+    }, 
+    methods:{
+        fetchTimeStamp(){
+
+                /** Fetch time stamp  */
+                /** compare with time stamp */
+                /** If needed stored the time stamp */
+                /** Mark the flag for sync */
+
+            fetch(
+                "http://vipslogic-local.no/rest/observation/organismsystemupdated"
+            )
+            .then ((response) => response.json())
+            .then((data) => {
+                this.strServerTimeStamp = data;
+                console.log('timestamp received : '+ this.strServerTimeStamp);
+            })
+
+        }
+    },
+    mounted(){
+        this.fetchTimeStamp();
+    }
+}
+</script>
