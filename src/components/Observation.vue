f2fd21db6ef3dca87e21c3184438b85ed385b9ae
Bhabesh <bhabesh.mukhopadhyay@nibio.no>
2021-02-24 20:42:12 +0100

    Get list of cropids related to crop category
    For selected observation get list of crop ids of related crop category. These ids needed for drop down to be created for crop

--- src/components/Observation.vue
+++ src/components/Observation.vue
@@ -2,16 +2,23 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <button type="button" class="btn btn-primary" id="cameraLauncher" @click="launchCamera">{{ take_photo }}</button>
+    <div>
+        
+    </div>
+    
   </div>
 </template>
 
 <script>
+import CommonUtil from '@/components/CommonUtil'
 export default {
   name: 'Observation',
+  props: ['observationId'],
   data () {
     return {
       msg: 'Observasjon',
-      take_photo: "Ta bilde"
+      take_photo: "Ta bilde",
+      observation:{}
     }
   },
   methods:{
@@ -28,10 +35,50 @@ export default {
 	            destinationType: Camera.DestinationType.FILE_URI ,
 	            correctOrientation: true
 	        });
-		}
+    },
+    getObservationFromStore(id)
+    {
+        let jsonObservation = {};
+
+        let lstObservations = JSON.parse(localStorage.getItem(CommonUtil.CONST_STORAGE_OBSERVATION_LIST));
+        jsonObservation = lstObservations.find(({observationId})=> observationId === id);
+        this.observation  = jsonObservation;
+
+        
+        /** Try to get list of crops from crop category ids */
+        let lstCropCategories = JSON.parse(localStorage.getItem(CommonUtil.CONST_STORAGE_CROP_CATEGORY));
+        //let testCropIds = lstObservations.find(({cropOrganismIds})=> cropOrganismIds === this.observation.cropOrganismId);
+
+      /** For Selected Observation We require List of related crop ids for dropdown   */
+      // Iterating crop categories
+       $.each(lstCropCategories, function(index,category){
+           // Iterating each category data
+           $.each(category, function(key, data){
+             //find the key meant for crop id/ids
+             if(key === 'cropOrganismIds')
+             {
+               // iterate and get the required cropids relaed to selected observation id
+                $.each (data, function(index, item){
+                    if(item===jsonObservation.cropOrganismId)
+                    {
+                      console.log('item : '+item+' -- data : '+JSON.stringify(data));
+                      return false;
+                    }
+                });
+             }
+           })
+        });
+
+       
+
+    }
   },
-  created () {
-    console.info("The observation component was initialized");
+  mounted(){
+    if(this.observationId)
+    {
+      this.getObservationFromStore(this.observationId);
+      console.log('observation id : '+this.observationId);
+    }
   }
 }
 </script>
