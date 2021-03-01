5e7edc6ab701609438070aa1255d73e043f13bcc
Bhabesh <bhabesh.mukhopadhyay@nibio.no>
2021-03-02 00:38:52 +0100

    Observation Form with Crop, Pest, Date, header and text

--- src/components/Observation.vue
+++ src/components/Observation.vue
@@ -2,23 +2,48 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <button type="button" class="btn btn-primary" id="cameraLauncher" @click="launchCamera">{{ take_photo }}</button>
+<br>
 
-
-    <div class="dropdown"></div >
+    <div class="btn-group">
         <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"  v-text="crop.cropName">
         Crops
         </button>
        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <a  class="dropdown-item" v-on:click="selectCrop(crop.organismId, crop.latinName)" href="#" v-for="crop in crops" v-bind:class="{ active: isActive }" >{{crop.latinName}}</a> 
+            <a  class="dropdown-item" v-on:click="selectCrop(crop.organismId, crop.latinName)" href="#" v-for="crop in crops" >{{crop.latinName}}</a> 
        </div>
-   
+    </div>
+    <br>
+      <div class="clearfix"/>
+    <div class="btn-group">
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"  v-text="pest.pestName">
+        Pests
+        </button>
+       <div class="dropdown-menu" aria-labelledby="dropdownMenuButton2">
+            <a  class="dropdown-item" v-on:click="selectPest(pest.pestId, pest.pestName)" href="#" v-for="pest in pests" >{{pest.pestName}}</a> 
+       </div>
+    </div>
+    <div class="clearfix"/>
+    <div>
+        {{strDateObservation | dateFormat}}
+    </div>
 
+    <div class="card">
+      <div class="card-header">
+        Observation Detail
+      </div>
+      <div class="card-body">
+        <h5 class="card-title text-success"><strong>{{observationHeader}}</strong></h5>
+        <p class="card-text">{{observationText}}</p>
+       
+      </div>
+    </div>
 
-  </div>
+  </div>  
 </template>
 
 <script>
 import CommonUtil from '@/components/CommonUtil'
+import { DateTime } from 'luxon'
 export default {
   name: 'Observation',
   props: ['observationId'],
@@ -28,8 +53,14 @@ export default {
       take_photo: "Ta bilde",
       observation:{},
       crops:[],
+      pests:[],
       crop:{},
-      isActive : false
+      pest:{},
+      isActive : false,
+      dateObservation : DateTime,
+      strDateObservation:'',
+      observationHeader : '',
+      observationText : ''
     }
   },
   methods:{
@@ -53,15 +84,19 @@ export default {
         if(id)
         {
               
-              
-              
               let jsonObservation = {};
-              
               
               let lstObservations = JSON.parse(localStorage.getItem(CommonUtil.CONST_STORAGE_OBSERVATION_LIST));              // Observation List
               jsonObservation     = lstObservations.find(({observationId})=> observationId === id);                           // Selection Observation
               this.observation    = jsonObservation;
+              /* For related Crop and Crop list */
               this.getObservationCrops(jsonObservation);
+              /* For related Pest and Pest list */
+              this.getObservationPests(jsonObservation);
+
+              this.strDateObservation = jsonObservation.timeOfObservation;
+              this.observationHeader = jsonObservation.observationHeading;
+              this.observationText  = jsonObservation.observationText;
 
           }
           else {
@@ -69,6 +104,7 @@ export default {
           }  
       },
 
+      /** Get related crop and crop list for a selected Observation */
       getObservationCrops(jsonObservation){
               let lstCrops        = [];
               let lstCropIds      = [];
@@ -99,16 +135,45 @@ export default {
                 })
               });
 
-
+            /* Get list of related crops with their id and name */
             $.each(lstCropIds, function(index, cropId){
-                //lstObservations.find(({observationId})=> observationId === id);   
                 let jsonDetailCrop  =   lstCropList.find(({organismId}) => organismId === cropId);
                 let jsonCrop        =   {'organismId':cropId, 'latinName':jsonDetailCrop.latinName};
                 lstCrops.push(jsonCrop);
             });
+
+
               let jsonSelectedCrop= lstCrops.find(({organismId}) => organismId === jsonObservation.cropOrganismId);
               this.crops = lstCrops;
               this.crop = {"cropId":jsonSelectedCrop.organismId, "cropName":jsonSelectedCrop.latinName};
+      },
+
+      getObservationPests(jsonObservation){
+            let lstPests            = [];
+            let lstPestIds          = [];
+            let lstPestList         = JSON.parse(localStorage.getItem(CommonUtil.CONST_STORAGE_PEST_LIST));
+            let lstCropPestList     = JSON.parse(localStorage.getItem(CommonUtil.CONST_STORAGE_CROP_PEST_LIST));
+ 
+             $.each(lstCropPestList, function(index,cropPests){
+                if(cropPests.cropOrganismId === jsonObservation.cropOrganismId)
+                {
+                    lstPestIds = cropPests.pestOrganismIds;
+                }
+                
+            }); 
+            
+            $.each(lstPestIds, function(index, pestId){
+                  let jsonDetailPest = lstPestList.find(({organismId}) => organismId === pestId);
+                  let jsonPest = {"pestId":jsonDetailPest.organismId, "pestName":jsonDetailPest.latinName};
+                  lstPests.push(jsonPest);
+
+            })
+
+            this.pests =  lstPests;
+
+            let jsonSelectedPest = lstPests.find(({pestId}) => pestId === jsonObservation.organismId);
+            this.pest = {"pestId":jsonSelectedPest.organismId, "pestName":jsonSelectedPest.pestName};
+
 
       },
 
@@ -116,11 +181,22 @@ export default {
       {
           this.crop={"cropId":cropId,"cropName":cropName};
          
+      },
+      selectPest(pestId, pestName)
+      {
+          this.pest={"pestId":pestId,"pestName":pestName};
+         
       }
 
       
 
   },
+  filters: {
+    dateFormat: function(timeStr) {
+      //return DateTime.fromISO(timeStr).toISODate();
+      return DateTime.fromISO(timeStr).toLocaleString(DateTime.DATETIME_MED);
+    }
+  },  
   mounted(){
     if(this.observationId)
     {
@@ -129,3 +205,7 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+
+</style>
