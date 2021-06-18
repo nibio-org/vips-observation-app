c6c50876e717ee0ac626536d32b21e404ca5aa2c
Bhabesh <bhabesh.mukhopadhyay@nibio.no>
2021-06-18 13:16:55 +0200

    VIPSLogic test server URL added

--- src/components/CommonUtil.vue
+++ src/components/CommonUtil.vue
@@ -35,7 +35,7 @@ export default {
     CONST_OBSERVATION_COUNT_START_ID    :    -1,
     CONST_POI_COUNT_START_ID            :    -1,
 
-    CONST_URL_DOMAIN                    :   'http://vipslogic-local.no',
+    CONST_URL_DOMAIN                    :   'http://logic.testvips.nibio.no', //'http://vipslogic-local.no',
 
     CONST_URL_AUTH_UUID                 :   '/rest/auth/uuid',
     CONST_URL_AUTH_LOGIN                :   '/rest/auth/login',
